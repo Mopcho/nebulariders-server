a6827278d2135f736cf0df62f2a43347347bbd5e
Mopcho <mopchowork@gmail.com>
2024-08-03 12:03:00 +0300

    extract http logic in a separate package

--- server/auth-server/main.go
+++ server/auth-server/main.go
@@ -4,111 +4,82 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Mopcho/nebulariders-server/auth/lib/mopHttp"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 )
 
 func (s *AuthService) handleVerifyToken(w http.ResponseWriter, r *http.Request) {
-	if (r.Method == "GET") {
-		token := r.URL.Query().Get("token")
-		parsedToken, err := verifyToken(token)
-		if err != nil {
-			w.WriteHeader(http.StatusUnauthorized)
-			sendResponse(w, ApiResponse{Error: &ApiError{Message: "Invalid Token"}})
-			return
-		}
-
-		subject, err := parsedToken.Claims.GetSubject()
-
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			sendResponse(w, ApiResponse{Error: &ApiError{Message:"Failed to get token subject"}})
-			return
-		}
-
-		// Check if user exists with this sub
-		userExists := false
-		for id := range s.Users {
-			user := s.Users[id]
-			if user.ID == subject {
-				userExists = true
-			}
-		}
-
-		if !userExists {
-			w.WriteHeader(http.StatusUnauthorized)
-			sendResponse(w, ApiResponse{Error: &ApiError{Message:"User for this token does not exist anymore"} })
-			return
-		}
-
-		sendResponse(w, ApiResponse{Data: "ok" })
+	token := r.URL.Query().Get("token")
+	parsedToken, err := verifyToken(token)
+	if err != nil {
+		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message: "Invalid Token"}}, http.StatusUnauthorized)
 		return
 	}
+	subject, err := parsedToken.Claims.GetSubject()
+	if err != nil {
+		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"Failed to get token subject"}}, http.StatusInternalServerError)
+		return
+	}
+	// Check if user exists with this sub
+	userExists := false
+	for id := range s.Users {
+		user := s.Users[id]
+		if user.ID == subject {
+			userExists = true
+		}
+	}
+	if !userExists {
+		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"User for this token does not exist anymore"} }, http.StatusUnauthorized)
+		return
+	}
+	mopHttp.SendResponse(w, mopHttp.ApiResponse{Data: "ok" }, http.StatusOK)
 }
 
 func (s *AuthService) handleLogin(w http.ResponseWriter, r *http.Request) {
-	if (r.Method == "POST") {
-		var decodedBody = LoginRequestBody{}
-		err := getJsonBody(w, r.Body, &decodedBody)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			sendResponse(w, ApiResponse{Error: &ApiError{Message:"Could not parse request body"}})
-			return
-		}
-
-		var user, ok = s.Users[decodedBody.Email]
-		if !ok {
-			w.WriteHeader(http.StatusUnauthorized)
-			sendResponse(w, ApiResponse{Error: &ApiError{Message:"Invalid Credentials"}})
-			return
-		}
-		
-		if user.Password != decodedBody.Password {
-			w.WriteHeader(http.StatusUnauthorized)
-			sendResponse(w, ApiResponse{Error: &ApiError{Message:"Invalid Credentials"}})
-			return
-		}
-		token, err := createToken(user.ID)
-
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			sendResponse(w, ApiResponse{Error: &ApiError{Message:"Internal Server Error"} })
-			return
-		}
-
-		type LoginResponseData struct { Token string `json:"token"`}
-		sendResponse(w, ApiResponse{Data: LoginResponseData{Token: token} })
+	var decodedBody = LoginRequestBody{}
+	err := mopHttp.GetJsonBody(w, r.Body, &decodedBody)
+	if err != nil {
+		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"Could not parse request body"}}, http.StatusInternalServerError)
 		return
 	}
+	var user, ok = s.Users[decodedBody.Email]
+	if !ok {
+		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"Invalid Credentials"}}, http.StatusUnauthorized)
+		return
+	}
+	
+	if user.Password != decodedBody.Password {
+		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"Invalid Credentials"}}, http.StatusUnauthorized)
+		return
+	}
+	token, err := createToken(user.ID)
+	if err != nil {
+		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"Internal Server Error"} }, http.StatusInternalServerError)
+		return
+	}
+	type LoginResponseData struct { Token string `json:"token"`}
+	mopHttp.SendResponse(w, mopHttp.ApiResponse{Data: LoginResponseData{Token: token} }, http.StatusOK)
 }
 
 func (s *AuthService) handleRegister(w http.ResponseWriter, r *http.Request) {
-	if (r.Method == "POST") {
-		var decodedBody = RegisterRequestBody{}
-		err := getJsonBody(w, r.Body, &decodedBody)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			sendResponse(w, ApiResponse{Error: &ApiError{Message:"Could not parse request body"}})
-		}
-
-		err = validateUserRegisterData(decodedBody)
-
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			sendResponse(w, ApiResponse{Error: &ApiError{Message: err.Error()}})
-			return
-		}
-
-		var _, ok = s.Users[decodedBody.Email]
-		if ok {
-			w.WriteHeader(http.StatusConflict)
-			sendResponse(w, ApiResponse{Error: &ApiError{Message:"User with this email already exists"}})
-			return
-		}
-
-		s.Users[decodedBody.Email] = &User{ID: uuid.New().String(),Username: decodedBody.Username, Email: decodedBody.Email, Password: decodedBody.Password}
-		sendResponse(w, ApiResponse{Data: "ok"})
-	}	
+	var decodedBody = RegisterRequestBody{}
+	err := mopHttp.GetJsonBody(w, r.Body, &decodedBody)
+	if err != nil {
+		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"Could not parse request body"}}, http.StatusInternalServerError)
+	}
+	err = validateUserRegisterData(decodedBody)
+	if err != nil {
+		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message: err.Error()}}, http.StatusBadRequest)
+		return
+	}
+	var _, ok = s.Users[decodedBody.Email]
+	if ok {
+		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"User with this email already exists"}}, http.StatusConflict)
+		return
+	}
+	s.Users[decodedBody.Email] = &User{ID: uuid.New().String(),Username: decodedBody.Username, Email: decodedBody.Email, Password: decodedBody.Password}
+	mopHttp.SendResponse(w, mopHttp.ApiResponse{Data: "ok"}, http.StatusOK)
 }
 
 func main() {
@@ -117,9 +88,9 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 	var authService = AuthService{Users: make(map[string]*User)} 
-	http.HandleFunc("/api/auth/login", authService.handleLogin)
-	http.HandleFunc("/api/auth/register", authService.handleRegister)
-	http.HandleFunc("/api/auth/verifyToken", authService.handleVerifyToken)
+	mopHttp.Post("/api/auth/login", authService.handleLogin)
+	mopHttp.Post("/api/auth/register", authService.handleRegister)
+	mopHttp.Post("/api/auth/verifyToken", authService.handleVerifyToken)
 	log.Println("Starting auth server on port 8081...")
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
--- server/auth-server/utils.go
+++ server/auth-server/utils.go
@@ -1,36 +1,12 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
-
-func sendResponse(w http.ResponseWriter, data ApiResponse) {
-	var bytes, err = json.Marshal(data)
-
-	if err != nil {
-		w.Write([]byte("Internal Server Error"))
-		return
-	}
-
-	w.Write(bytes)
-}
-
-func getJsonBody(w http.ResponseWriter, body io.ReadCloser, v any) error {
-	var err = json.NewDecoder(body).Decode(&v)
-	if err != nil {
-		return errors.New("failed decoding request body")
-	}
-
-	return nil
-}
 
 func createToken(sub string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
