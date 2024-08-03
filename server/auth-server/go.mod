b9b17d8d26faca561c51b40bf48bba49776c64eb
Mopcho <mopchowork@gmail.com>
2024-08-03 15:06:09 +0300

    Implement hashing with bcrypt

--- server/auth-server/go.mod
+++ server/auth-server/go.mod
@@ -8,3 +8,5 @@ require (
 )
 
 require github.com/joho/godotenv v1.5.1
+
+require golang.org/x/crypto v0.25.0
--- server/auth-server/main.go
+++ server/auth-server/main.go
@@ -16,11 +16,13 @@ func (s *AuthService) handleVerifyToken(w http.ResponseWriter, r *http.Request) 
 		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message: "Invalid Token"}}, http.StatusUnauthorized)
 		return
 	}
+
 	subject, err := parsedToken.Claims.GetSubject()
 	if err != nil {
 		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"Failed to get token subject"}}, http.StatusInternalServerError)
 		return
 	}
+
 	// Check if user exists with this sub
 	userExists := false
 	for id := range s.Users {
@@ -29,56 +31,68 @@ func (s *AuthService) handleVerifyToken(w http.ResponseWriter, r *http.Request) 
 			userExists = true
 		}
 	}
+
 	if !userExists {
 		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"User for this token does not exist anymore"} }, http.StatusUnauthorized)
 		return
 	}
+
 	mopHttp.SendResponse(w, mopHttp.ApiResponse{Data: "ok" }, http.StatusOK)
 }
 
 func (s *AuthService) handleLogin(w http.ResponseWriter, r *http.Request) {
 	var decodedBody = LoginRequestBody{}
-	err := mopHttp.GetJsonBody(w, r.Body, &decodedBody)
-	if err != nil {
+
+	if err := mopHttp.GetJsonBody(w, r.Body, &decodedBody); err != nil {
 		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"Could not parse request body"}}, http.StatusInternalServerError)
 		return
 	}
+
 	var user, ok = s.Users[decodedBody.Email]
 	if !ok {
 		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"Invalid Credentials"}}, http.StatusUnauthorized)
 		return
 	}
 	
-	if user.Password != decodedBody.Password {
+	if 	ok := CheckPasswordHash(decodedBody.Password, user.Password); !ok {
 		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"Invalid Credentials"}}, http.StatusUnauthorized)
 		return
 	}
+
 	token, err := createToken(user.ID)
 	if err != nil {
 		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"Internal Server Error"} }, http.StatusInternalServerError)
 		return
 	}
+
 	type LoginResponseData struct { Token string `json:"token"`}
 	mopHttp.SendResponse(w, mopHttp.ApiResponse{Data: LoginResponseData{Token: token} }, http.StatusOK)
 }
 
 func (s *AuthService) handleRegister(w http.ResponseWriter, r *http.Request) {
 	var decodedBody = RegisterRequestBody{}
-	err := mopHttp.GetJsonBody(w, r.Body, &decodedBody)
-	if err != nil {
+	
+	if err := mopHttp.GetJsonBody(w, r.Body, &decodedBody); err != nil {
 		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"Could not parse request body"}}, http.StatusInternalServerError)
 	}
-	err = validateUserRegisterData(decodedBody)
-	if err != nil {
+	
+	if err := validateUserRegisterData(decodedBody); err != nil {
 		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message: err.Error()}}, http.StatusBadRequest)
 		return
 	}
-	var _, ok = s.Users[decodedBody.Email]
-	if ok {
+	
+	if _, ok := s.Users[decodedBody.Email]; ok {
 		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"User with this email already exists"}}, http.StatusConflict)
 		return
 	}
-	s.Users[decodedBody.Email] = &User{ID: uuid.New().String(),Username: decodedBody.Username, Email: decodedBody.Email, Password: decodedBody.Password}
+
+	hashedPass, err := hashPassword(decodedBody.Password)
+	if err != nil {
+		mopHttp.SendResponse(w, mopHttp.ApiResponse{Error: &mopHttp.ApiError{Message:"Failed hashing password"}}, http.StatusInternalServerError)
+		return
+	}
+
+	s.Users[decodedBody.Email] = &User{ID: uuid.New().String(),Username: decodedBody.Username, Email: decodedBody.Email, Password: hashedPass}
 	mopHttp.SendResponse(w, mopHttp.ApiResponse{Data: "ok"}, http.StatusOK)
 }
 
