c20f83f40c6873aac7020265d8fadfa1268c4287
Stoyan Kolev <stoyan.a.kolev@gmail.com>
2024-08-10 01:00:31 +0300

    implement smoother movement and rotation

--- client/world/Entities.gd
+++ client/world/Entities.gd
@@ -1,22 +1,23 @@
 extends Node2D
 
-const PLAYER_ID = "b6023e05-5aec-40b8-9500-9d6f3b949d7c";
-
 var OtherPlayer = preload("res://world/OtherPlayer.tscn")
 
 var ENTITIES = {}
 
 func _on_world_websocket_packet(packed: PackedByteArray):
 	var message = JSON.parse_string(packed.get_string_from_utf8())
-	var players = message["players"]
+	var players = message["data"]["players"]
 	
 	for playerId in players:
-		if (playerId == PLAYER_ID):
-			continue
 		if !ENTITIES.has(playerId):
+			print(message)
+			var playerData = players[playerId];
 			var otherPlayerInstance = OtherPlayer.instantiate()
-			ENTITIES[playerId] = otherPlayerInstance
+			otherPlayerInstance.setup(playerData["username"])
+			ENTITIES[playerId] = { "instance": otherPlayerInstance, "data": playerData }
 			add_child(otherPlayerInstance)
 		var server_player = players[playerId]
 		var local_player = ENTITIES[playerId]
-		local_player.position = Vector2(server_player["x"], server_player["y"])
+		var next_position = Vector2(server_player["x"], server_player["y"])
+		
+		local_player.instance.move_to_position(next_position)
